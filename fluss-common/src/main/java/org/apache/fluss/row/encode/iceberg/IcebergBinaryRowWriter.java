5967dd196c5a2eddace747374494fba8ffd769ec
pithecuse527 <gihong96@gmail.com>
2026-01-23 19:53:17 -0500

    test

--- fluss-common/src/main/java/org/apache/fluss/row/encode/iceberg/IcebergBinaryRowWriter.java
+++ fluss-common/src/main/java/org/apache/fluss/row/encode/iceberg/IcebergBinaryRowWriter.java
@@ -203,7 +203,7 @@ class IcebergBinaryRowWriter {
             case DOUBLE:
                 return (writer, value) -> writer.writeDouble((double) value);
 
-            // support for nanoseconds come check again after #1195 merge
+                // support for nanoseconds come check again after #1195 merge
             case TIMESTAMP_WITHOUT_TIME_ZONE:
                 return (writer, value) -> {
                     TimestampNtz ts = (TimestampNtz) value;
